be61f5aad9ecc6097cfc3c891ed550c9ac5d497f
Voltasalt <voltasalt@gmail.com>
2015-12-10 22:55:06 +0100

    Day 1, Part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -2,18 +2,28 @@ use std::io::{self, Read};
 
 fn main() {
     println!("Accepting input from stdin (unbuffered, so pipe input)");
-    
+
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
 
     let mut counter = 0;
-    for c in buffer.chars() {
+    let mut basement_position = 0;
+    for (i, c) in buffer.chars().enumerate() {
         match c {
             '(' => counter += 1,
-            ')' => counter -= 1,
+            ')' => {
+                counter -= 1;
+                if counter == -1 && basement_position == 0 {
+                    basement_position = i + 1;
+                }
+            },
             _ => {}
         }
     }
 
-    println!(" - Santa is on floor {} -", counter)
+    println!(" - Santa is on floor {} -", counter);
+
+    if basement_position > 0 {
+        println!(" - Santa entered the basement at position {} -", basement_position);
+    }
 }
