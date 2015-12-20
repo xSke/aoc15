69da8b2b792f71f7f4af967905d1be2852da5904
Voltasalt <voltasalt@gmail.com>
2015-12-20 12:27:08 +0100

    Day 20

---
+++ src/bin/day20_2.rs
@@ -0,0 +1,37 @@
+use std::io::{self, Write, BufRead};
+use std::str::FromStr;
+
+fn main() {
+    println!("What's the input?");
+    print!("> ");
+    io::stdout().flush().unwrap();
+
+    let stdin = io::stdin();
+    let input_str = stdin.lock().lines().next().unwrap().unwrap();
+    let input = usize::from_str(&input_str).unwrap();
+    let input2 = input / 11;
+
+    let mut houses = vec![0; input2 as usize];
+
+    'outer: for num in 1..input2 {
+        // Range::step_by is unstable :(
+
+        let mut i = 0usize;
+        let mut ii = 0;
+        while i < input2 {
+            i += num;
+
+            if i < input2 {
+                houses[i] += num;
+            }
+
+            ii += 1;
+            if ii >= 50 {
+                break;
+            }
+        }
+    }
+
+    let out = houses.iter().enumerate().skip_while(|x| *x.1 < input2).next().unwrap().0;
+    println!(" - The first house with {} or more presents is house #{} -", input, out);
+}
